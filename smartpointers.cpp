f28048d6d4cbaed63024f8954d3adf242d78a77c
asbtho <a_thom89@hotmail.com>
2024-09-16 23:38:23 +0200

    gsddaga

---
+++ smartpointers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <memory>
+
+class Entity{
+public:
+    Entity(){ // Constructor
+        std::cout << "Created Entity!" << std::endl;
+    }
+
+    ~Entity(){ // Destructor
+        std::cout << "Destroyed Entity!" << std::endl;
+    }
+};
+
+int main(){
+    std::unique_ptr<Entity> entity = std::make_unique<Entity>();
+    std::shared_ptr<Entity> sharedEntity = std::make_shared<Entity>();
+
+    std::cin.get();
+} 
